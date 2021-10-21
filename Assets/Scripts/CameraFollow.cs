329701f5e1dd74a2880d9f7c7d91b15bce518571
ibrahim <ibrahim74115@gmail.com>
2021-10-21 10:36:58 -0400

    Add camera boundaries to world back

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,17 +7,33 @@ public class CameraFollow : MonoBehaviour
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
+    public Transform northBoundary;
+    public Transform eastBoundary;
+    public Transform southBoundary;
+    public Transform westBoundary;
+
     private Transform target;
+
+    private float halfHeight;
+    private float halfWidth;
 
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = target.position + offset;
+        Vector3 temp = target.position + offset;
+        temp.x = Mathf.Clamp(temp.x, westBoundary.position.x + halfWidth, eastBoundary.position.x - halfWidth);
+        temp.y = Mathf.Clamp(temp.y, southBoundary.position.y + halfHeight, northBoundary.position.y - halfHeight);
+
+
+        transform.position = temp;
     }
 }
