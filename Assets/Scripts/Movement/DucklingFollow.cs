bb7ed8c47fc8b36864112293c30692827e502db1
Hong-Hue Tran <hong.hue.tran@outlook.com>
2021-10-24 00:39:50 -0400

    ducking drop mechanic

--- Assets/Scripts/Movement/DucklingFollow.cs
+++ Assets/Scripts/Movement/DucklingFollow.cs
@@ -11,6 +11,7 @@ public class DucklingFollow : DuckMovement
     private Transform target;
     private float duckCount;
     private float followDistance;
+    private float dropRadius;
 
     void Start()
     {
@@ -37,22 +38,37 @@ public class DucklingFollow : DuckMovement
     void Update()
     {
         if(follow) {
-            //follow the player
-            if(Vector2.Distance(transform.position, target.position) > followDistance) {
+            if(Vector2.Distance(transform.position, target.position) > dropRadius) {
+                duckling.gameObject.tag = "Duckling";
+                duckling.gameObject.GetComponent<BoxCollider2D>().enabled = true;
+                //get and update duckling count
+                duckCount = PlayerPrefs.GetFloat("duckCount");
+                PlayerPrefs.SetFloat("duckCount", duckCount-1);
+                follow = false;
+            }          
+            else if(Vector2.Distance(transform.position, target.position) > followDistance) {
+                //follow the player
                 transform.position = Vector2.MoveTowards(transform.position, target.position, followSpeed * Time.deltaTime);
             }
         }
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player" && duckling.gameObject.tag == "Duckling") {
+            //change duckling tag to "IncludedDuckling"
             duckling.gameObject.tag = "IncludedDuckling";
+            //disable boxCollider on duckling
             duckling.gameObject.GetComponent<BoxCollider2D>().enabled = false;
 
+            //get and update duckling count
             duckCount = PlayerPrefs.GetFloat("duckCount");
-            PlayerPrefs.SetFloat("duckCount", duckCount+0.75f);
-            followDistance = PlayerPrefs.GetFloat("duckCount");
+            PlayerPrefs.SetFloat("duckCount", duckCount+1);
+            //set followDistance
+            followDistance = PlayerPrefs.GetFloat("duckCount")*0.75f;
+            //set dropRadius
+            dropRadius = followDistance + 2;
 
             follow = true;
         }
