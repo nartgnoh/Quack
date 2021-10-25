64310a27bb80a59021cfff907b0d579c1c8b075e
thebatmanyo <62774470+thebatmanyo@users.noreply.github.com>
2021-10-24 22:57:38 -0400

    Music

---
+++ Assets/Scripts/Camera/MusicController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public GameObject music;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        music.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
